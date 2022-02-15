cc52c1c0bb637372d26bb5bf324bd369ac366150
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-15 16:10:11 +0200

    Edit signup function

--- Javascript/signup.js
+++ Javascript/signup.js
@@ -1,16 +1,4 @@
-let signin_api = "http://localhost/fsw-facebook-clone-backend/PHP/signin-api.php";
 let signup_api = "http://localhost/fsw-facebook-clone-backend/PHP/signup-api.php";
-
-
-let first_name = document.getElementById("first_name").value;
-let last_name = document.getElementById("last_name").value;
-let dob = document.getElementById("dob").value;
-let email = document.getElementById("email").value;
-let password = document.getElementById("password").value;
-let profile_picture = document.getElementById("profile_picture").value;
-let country = document.getElementById("country").value;
-let city = document.getElementById("city").value;
-let street = document.getElementById("street").value;
 
 const signUp = async (first_name, last_name, dob, email, password, profile_picture, country, city, street) => {
     const response = await fetch(signup_api, {
@@ -31,12 +19,26 @@ const signUp = async (first_name, last_name, dob, email, password, profile_pictu
         })
     });
     const token = await response.json();
-    return token;
-};
-if(signUp(first_name, last_name, dob, email, password, profile_picture, country, city, street) != null){
-    signUp(first_name, last_name, dob, email, password, profile_picture, country, city, street).then((data) => {
-        token = data.token;
+    if (json_object.status == "Welcome to Facebook"){
+        token = json_object.token;
         localStorage.setItem("id", token);
         location.href = "http://localhost/fsw-facebook-clone-frontend/HTML/homepage.html";
-      });
-}
+        return token;
+    }else{
+        console.log(json_object.status);
+    }
+};
+
+document.getElementById("createaccount_button").addEventListener("click", function(){
+    let first_name = document.getElementById("first_name").value;
+    let last_name = document.getElementById("last_name").value;
+    let dob = document.getElementById("dob").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let profile_picture = document.getElementById("profile_picture").value;
+    let country = document.getElementById("country").value;
+    let city = document.getElementById("city").value;
+    let street = document.getElementById("street").value;  
+    signUp(first_name, last_name, dob, email, password, profile_picture, country, city, street);
+});
+
