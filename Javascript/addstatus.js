2b3fa1f6266378d9b3b7a1aec36abbb6e1d7e17d
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-15 21:49:46 +0200

    Edit if statement and json response

--- Javascript/addstatus.js
+++ Javascript/addstatus.js
@@ -1,25 +1,31 @@
 let addstatus_api = "http://localhost/fsw-facebook-clone-backend/PHP/status-api.php";
 
 const addStatus = async (id, post) => {
+    
     const response = await fetch(addstatus_api, {
         method: "POST",
         headers: new Headers({
             "Content-Type": "application/json"
         })
         ,body: JSON.stringify({
-            user_id: id,
+            id: id,
             post: post
         })
     });
+    
     const json_object = await response.json();
+    
     if (json_object.status == "Post added successfully"){
-        document.getElementById("feed").appendChild(json_object.post);
+        post_id = json_object.post_id;
+        timestamp = json_object.timestamp;
+        document.getElementById("feed").innerHTML += `<div class="posts" id="post_${post_id}">${post}<br/>${timestamp}</div>`;
     }else{
         console.log(json_object.status);
-    }};
+    }
+};
 
     document.getElementById("post_button").addEventListener("click", function(){
         let id = localStorage.getItem("id");
-        let post = document.getElementById("add_status");  
+        let post = document.getElementById("post_status").value;  
         addStatus(id, post);
     });
