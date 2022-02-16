5a4ea2e7ba93acdfcce60676ae7cce840bd36aff
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-16 02:08:53 +0200

    Loop over json object

--- Javascript/friendstatus.js
+++ Javascript/friendstatus.js
@@ -12,13 +12,27 @@ const friendStatus = async (id) => {
             id: id
         })
     });
-    const token = await response.json();
-    return token;
+    const json_object = await response.json();
+
+    if (json_object.status == "User not found"){
+        console.log(json_object.status);
+    }else{
+        post_id = json_object.post_id;
+        timestamp = json_object.timestamp;
+        
+        for(let i in json_object){
+            let first_name = json_object[i].first_name;
+            let last_name = json_object[i].last_name;
+            let post = json_object[i].post;
+            let timestamp = json_object[i].timestamp;
+            document.getElementById("feed").innerHTML += 
+            `<div class="fullname">${first_name} ${last_name}</div>
+             <div class="posts" id="post_${post_id}">${post}</div>
+             <div class="timestamps" id="timestamp_${post_id}">${timestamp}</div>`;
+        }
+    }
 };
-if(friendStatus(id) != null){
-    friendStatus(id).then((data) => {
-        data.forEach((element) => {
-          document.getElementById("feed").innerHTML += `<div class="posts" id="post_${element.id}">${element}</div>`;
-        });                                             
-    });        
-};
+
+window.addEventListener("load",function(){
+    friendStatus(id);
+});
