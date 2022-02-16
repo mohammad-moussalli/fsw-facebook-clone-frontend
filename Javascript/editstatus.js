3b96e944188757907999f9388a98f4658432f6b4
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-16 03:21:35 +0200

    Add event listener

--- Javascript/editstatus.js
+++ Javascript/editstatus.js
@@ -1,31 +1,45 @@
 let editstatus_api = "http://localhost/fsw-facebook-clone-backend/PHP/edit-status-api.php";
 
-let post = document.getElementById(`post_${post_id}`);
-let id = localStorage.getItem("id");
-
-const editStatus = async (post_id, post, id) => {
+const editStatus = async (post, id, post_id) => {
     const response = await fetch(editstatus_api, {
         method: "POST",
         headers: new Headers({
             "Content-Type": "application/json"
         })
         ,body: JSON.stringify({
-            post_id: post_id,
             post: post,
-            user_id: id
+            user_id: id,
+            post_id: post_id
         })
     });
-    const token = await response.json();
-    return token;
-};
-if(editStatus(post_id, post, id) != null){
+    const json_object = await response.json();
+    if (json_object.status == "Status updated"){
+        post_id = json_object.post_id;
+        updated_post = json_object.post;
+        timestamp = json_object.timestamp;
 
-    editStatus(post_id, post, id).then((data) => {
-        post_id = data.post_id;
-        post = data.post;
-        document.getElementById(`edit_button${post_id}`).addEventListener("click", function(){
-            document.getElementById(`post_${post_id}`).innerHTML = post;
-        })        
+        document.getElementById(`post_${post_id}`).innerHTML = updated_post;
+        document.getElementById(`post_${post_id}`).innerHTML += timestamp;
+    }else{
+        console.log(json_object.status);
+    }
+};
+
+document.querySelectorAll("edit_buttons").forEach((button) => {
+    let post_id = button.id.replace(/\D/g, "");
+    console.log(post_id);
+    document.getElementById(button.id).addEventListener("click", function(){
+        let post = document.getElementById(`post_${id}`)
+        post.contentEditable = true;
+        let id = localStorage.getItem("id");
+        editStatus(post, id, post_id);
     });
-    
-}
+
+});
+
+document.querySelectorAll("submit_buttons").forEach((button) => {
+    document.getElementById(button.id).addEventListener("click", function(){
+        edited_post.contentEditable = false;
+    });
+});
+   
